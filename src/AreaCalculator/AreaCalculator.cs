0f0a2b7a7e5350259f585cccbbebf1b02d4c2e05
kellhusCorp <alkan_7@mail.ru>
2023-01-29 15:13:22 +0400

    Добавил обертку для вычислений площади фигур + тест.

---
+++ src/AreaCalculator/AreaCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AreaCalculator.Abstractions;
+
+namespace AreaCalculator
+{
+    public static class AreaCalculator
+    {
+        public static double GetArea(Figure figure)
+        {
+            return figure.GetArea();
+        }
+
+        public static double GetTotalArea(IEnumerable<Figure> figures)
+        {
+            return figures.Sum(figure => figure.GetArea());
+        }
+    }
+}
---
+++ tests/AreaCalculator.Tests/AreaCalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using AreaCalculator.Abstractions;
+using AreaCalculator.Figures;
+
+namespace AreaCalculator.Tests;
+
+public class AreaCalculatorTests : BaseTests
+{
+    [Test]
+    public void Test_AreaCalculator_GetTotalArea()
+    {
+        var figures = new List<Figure>
+        {
+            new Circle(3d),
+            new Triangle(3d, 4d, 5d),
+            new EquilateralTriangle(3d)
+        };
+
+        var totalArea = AreaCalculator.GetTotalArea(figures);
+        
+        Assert.That(totalArea, Is.EqualTo(38.1714478823081379).Within(1E-5));
+    }
+}
