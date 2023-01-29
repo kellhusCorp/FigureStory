2e19ddeb07200214ad82b13a853d236b4973d1b1
kellhusCorp <alkan_7@mail.ru>
2023-01-29 16:15:42 +0400

    Добавил пример для добавления фигуры.

---
+++ samples/Sample/Figures/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using AreaCalculator.Abstractions;
+
+namespace Sample.Figures;
+
+// Пример добавления фигуры.
+
+/// <summary>
+/// Прямоугольник.
+/// </summary>
+public class Rectangle : Figure
+{
+    private readonly double width;
+    private readonly double height;
+
+    
+    public Rectangle(double width, double height)
+    {
+        if (width < 0)
+        {
+            throw new ArgumentException("Ширина не может быть меньше 0");
+        }
+
+        if (height < 0)
+        {
+            throw new ArgumentException("Высота не может быть меньше 0");
+        }
+        
+        this.width = width;
+        this.height = height;
+    }
+    
+    public override double GetArea()
+    {
+        return width * height;
+    }
+}
